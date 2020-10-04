a04178017105fd5705e9308fe0bd76d702f60090
Saleh Alibrahim <s.albrahim@outlook.com>
2020-10-04 20:16:05 +0300

    fix: first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+const api = require('./src/api');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
+  });
+});
+
+
+
+app.use('/api/v1', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening: http://localhost:${port}`);
+});
+
+
+module.exports = app;
